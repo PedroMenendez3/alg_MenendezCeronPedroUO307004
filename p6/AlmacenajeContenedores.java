1d7e7fc9914c3f44af45a9925fe19034dca18877
Pedro Menéndez Cerón <UO307004@uniovi.es>
2026-03-25 20:03:19 +0100

    Trabajo clase p6

---
+++ p6/AlmacenajeContenedores.java
@@ -0,0 +1,130 @@
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class AlmacenajeContenedores {
+    
+    private int capacidadC;
+    private Integer[] conjuntoS;
+    private int mejorK; //Numero minimo de contenedores
+    private List<List<Integer>> mejorDistribucion; 
+
+    public AlmacenajeContenedores(int capacidadC, Integer[] conjuntoS){
+        this.capacidadC = capacidadC;
+        this.conjuntoS = conjuntoS;
+        Arrays.sort(this.conjuntoS, Collections.reverseOrder());
+        this.mejorK = conjuntoS.length;
+    }
+
+    public void resolver(){
+        //ordenar descendente (ya se hace en el constructor)
+
+        List<List<Integer>> contenedores = new ArrayList<>();
+        backTracking(0, contenedores);
+        //mostrar solucion
+        imprimirSolucion();
+    }
+
+    private void backTracking(int indexObject, List<List<Integer>> contenedores){
+        //Caso base
+        if(indexObject == conjuntoS.length){
+            if(contenedores.size() < mejorK){
+                mejorK = contenedores.size();
+                mejorDistribucion = copiar(contenedores);
+            }
+            return; //OJO muy importante este return para que no se siga con la ejecucion estando ya en el caso base
+        }
+
+        //Podamos if(size contenedores) > mejorK paramos
+        if(contenedores.size() >= mejorK){
+            return;
+        }
+
+        //Probar a meter en contenedores existentes 
+        for(int i = 0; i < contenedores.size(); i++){
+            if(sum(contenedores.get(i)) + conjuntoS[indexObject] <= capacidadC){
+                // Avanzar
+                contenedores.get(i).add(conjuntoS[indexObject]);
+                //backTracking
+                backTracking(indexObject + 1, contenedores);
+                //Retroceder
+                contenedores.get(i).remove(contenedores.get(i).size() -1);
+            }
+        }
+
+        //Intentar meterlo en un nuevo contenedor
+        List<Integer> nuevoContenedor = new ArrayList<>();
+        nuevoContenedor.add(conjuntoS[indexObject]);
+        //Avanzo
+        contenedores.add(nuevoContenedor);
+        //backTracking
+        backTracking(indexObject + 1, contenedores);
+        //Retrocedo
+        contenedores.remove(contenedores.size() - 1);
+        
+    }
+
+    private List<List<Integer>> copiar(List<List<Integer>> contenedores){
+        List<List<Integer>> copia = new ArrayList<>();
+        for(List<Integer> i : contenedores){
+            copia.add(i);
+        }
+        return copia;
+    }
+
+    private Integer sum(List<Integer> contenedor){
+        Integer sumatorio= 0;
+        for(int i = 0; i < contenedor.size(); i++){
+            sumatorio = sumatorio + contenedor.get(i);
+        }
+        return sumatorio;
+    }
+
+    private void imprimirSolucion(){
+        System.out.println("Lista de contenedores y objetos contenidos: ");
+        for(int i = 0; i < mejorDistribucion.size(); i++){
+            System.out.print("Contenedor " + i + ": ");
+            for(int j = 0; j < mejorDistribucion.get(i).size(); j++){
+                System.out.print(mejorDistribucion.get(i).get(j) + " ");
+            }
+            System.out.println("");
+        }
+
+        System.out.println("El número de contenedores necesario es " + mejorK);
+    }
+
+    public static void main(String[] args){
+        try {
+            Scanner sc;
+            sc = new Scanner(new FileReader(args[0]));
+            int capacidadC = sc.nextInt();
+            String[] parts = sc.nextLine().split(" ");
+            Integer[] toS = new Integer[parts.length];
+            
+            int i = 0;
+            for(String s : parts){
+                toS[i] = Integer.parseInt(s);
+                i++;
+            }
+            
+            sc.close();
+            new AlmacenajeContenedores(capacidadC,toS).resolver();
+        } catch (FileNotFoundException ex) {
+            System.out.println("Reventó");
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
